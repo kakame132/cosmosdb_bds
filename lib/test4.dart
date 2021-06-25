916a350959fb888ce69f0c481c00c09457e2401e
kakame132 <67035034+kakame132@users.noreply.github.com>
2021-06-25 17:29:32 +0700

    Add files via upload

--- lib/test4.dart
+++ lib/test4.dart
@@ -147,9 +147,10 @@ List get_input(List c){
   print("*****"); 
   print(final_keys);
   print(final_values);
+
   List result=[];
-  for(int i=0;i<keys.length;i++){
-    Map x={keys[i]:values[i]};
+  for(int i=0;i<final_keys.length;i++){
+    Map x={final_keys[i]:final_values[i]};
     result.add(x);
   }
   return result;
